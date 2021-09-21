7a7b159c7f79ce9a98ad05503810eefce74d7d73
TraistaRafael <traista.rafael@gmail.com>
2021-09-21 23:58:24 +0300

    Removed GeneratorTest development folder.

--- GeneratorTest/Animal.h
+++
@@ -1,9 +0,0 @@
-class Animal{
-    Animal(){
-        list = new float[10];
-        list[0] = 1;
-        list[1] = 2;
-        list[2] = 3;
-    }
-    float* list;
-};
--- GeneratorTest/recast/Native.hx
+++
@@ -1,3 +0,0 @@
-package recast;
-
-typedef Native = haxe.macro.MacroType<[webidl.Module.build({ idlFile : "recast.idl", autoGC : true, nativeLib : "recast" })]>;
