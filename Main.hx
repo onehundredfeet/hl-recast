29a0d4223519a9471320ba784a0b71658a6fdb06
Ryan Cleven <rcleven@mac.com>
2021-09-25 20:05:32 -0700

    Used Vectors has parameters
    Also fixed arrays

--- Main.hx
+++ Main.hx
@@ -8,8 +8,8 @@ class Main {
 
 		trace("vectors");
 		get_set_rcConfig_bounds();
-		// test_rcCalcBounds_0();
-		// test_rcCalcBounds_1();
+		 test_rcCalcBounds_0();
+		 test_rcCalcBounds_1();
 	}
 
 	public static function test_DetourMath_0(){
@@ -45,27 +45,30 @@ class Main {
 		trace("Done");
 	}
 
+	// Converted to new method
 	public static function test_rcCalcBounds_0(){
 		// bounds of one vector
 	
 		
-		var verts = new recast.Native.FloatArray(3);
-		var bmin = new recast.Native.FloatArray(3);
-		var bmax = new recast.Native.FloatArray(3);
-		verts.Set(0, 1);
-		verts.Set(1, 2);
-		verts.Set(2, 3);
+		var verts = new hl.NativeArray<Single>(3);
+		var bmin = new hl.NativeArray<Single>(3);
+		var bmax = new hl.NativeArray<Single>(3);
+		verts[0] = 1;
+		verts[1] = 2;
+		verts[2] = 3;
+		
+
 		recast.Native.Recast.rcCalcBounds(verts, 1, bmin, bmax);
 		
 		var test = true;
 
-		test = test && bmin.Get(0) == verts.Get(0);
-		test = test && bmin.Get(1) == verts.Get(1);
-		test = test && bmin.Get(2) == verts.Get(2);
+		test = test && bmin[0] == verts[0];
+		test = test && bmin[1] == verts[1];
+		test = test && bmin[2] == verts[2];
 
-		test = test && bmax.Get(0) == verts.Get(0);
-		test = test && bmax.Get(1) == verts.Get(1);
-		test = test && bmax.Get(2) == verts.Get(2);
+		test = test && bmax[0] == verts[0];
+		test = test && bmax[1] == verts[1];
+		test = test && bmax[2] == verts[2];
 
 		trace("rcCalcBounds: bounds of one vector");
 		trace(test);
@@ -74,26 +77,26 @@ class Main {
 	public static function test_rcCalcBounds_1(){
 		// bounds of more than one vector
 
-		var verts = new recast.Native.FloatArray(6);
-		var bmin = new recast.Native.FloatArray(3);
-		var bmax = new recast.Native.FloatArray(3);
-		verts.Set(0, 1);
-		verts.Set(1, 2);
-		verts.Set(2, 3);
-		verts.Set(3, 0);
-		verts.Set(4, 2);
-		verts.Set(5, 5);
+		var verts = new hl.NativeArray<Single>(6);
+		var bmin = new hl.NativeArray<Single>(3);
+		var bmax = new hl.NativeArray<Single>(3);
+		verts[0] = 1;
+		verts[1] =  2;
+		verts[2] =  3;
+		verts[3] =  0;
+		verts[4] =  2;
+		verts[5] =  5;
 		recast.Native.Recast.rcCalcBounds(verts, 2, bmin, bmax);
 
 		var test = true;
 
-		test = test && bmin.Get(0) == 0.0;
-		test = test && bmin.Get(1) == 2.0;
-		test = test && bmin.Get(2) == 3.0;
+		test = test && bmin[0] == 0.0;
+		test = test && bmin[1] == 2.0;
+		test = test && bmin[2] == 3.0;
 
-		test = test && bmax.Get(0) == 1.0;
-		test = test && bmax.Get(1) == 2.0;
-		test = test && bmax.Get(2) == 5.0;
+		test = test && bmax[0] == 1.0;
+		test = test && bmax[1] == 2.0;
+		test = test && bmax[2] == 5.0;
 
 		trace("rcCalcBounds: bounds of more than one vector");
 		trace(test);
