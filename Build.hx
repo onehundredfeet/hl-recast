716abbebc77bb39e82886d6974711b2042fe40c7
Ryan Cleven <rcleven@mac.com>
2024-03-24 21:07:31 -0700

    Fixed latest IDL break

--- Build.hx
+++ Build.hx
@@ -86,12 +86,8 @@ rcSpan* rcHeightfield_rcSpanAt(rcHeightfield *context, int pos) {
 ";
 
 
-	static var hlDir = null;
 	public static function main() {
 		var b = new Build();
-					case "--hl":
-						hlDir = args.shift();
-
 		b.parseArgs();
 
 	}
